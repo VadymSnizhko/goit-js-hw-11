baa435d175e90d66a0fb26bba4bece131dbec1ce
vadim <snizhko@gmail.com>
2025-04-01 23:51:06 +0300

    create projectBase HW 11

---
+++ src/js/render-functions.js
@@ -0,0 +1,9 @@
+import SimpleLightbox from 'simplelightbox';
+
+function createGallery(images) {}
+
+function clearGallery() {}
+
+function showLoader() {}
+
+function hideLoader() {}
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import getImagesByQuery from './js/pixabay-api';
+import createGallery from './js/render-functions';
+import clearGallery from './js/render-functions';
+import showLoader from './js/render-functions';
+import hideLoader from './js/render-functions';
