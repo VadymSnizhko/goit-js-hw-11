18a02307ceef8a3ea8dbd9e7e06cf2c08b6c4fe6
vadim <snizhko@gmail.com>
2025-04-04 04:35:56 +0300

    whisout loader HW 11

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,3 +1,10 @@
-function getImagesByQuery(query) {
-  return;
+import axios from 'axios';
+//повинна прибирати клас для відображення лоадера
+import { hideLoader } from './render-functions';
+
+export default function getImagesByQuery(query) {
+  return axios(query)
+    .then(response => response.data)
+    .catch(error => null)
+    .finally(() => hideLoader());
 }
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,9 +1,50 @@
+// Описаний в документації
 import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-function createGallery(images) {}
+const galleryList = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
 
-function clearGallery() {}
+export function createGallery(images) {
+  const htmlImage = images
+    .map(
+      item => `<li class="gallery-cart">
+      <a href=${item.largeImageURL}>
+      <img class="gallery-image" src=${item.webformatURL} alt=${item.tags}></img>
+      <div class="stats">
+        <span>Like</span>
+        <span>Views</span>
+        <span>Comments</span>
+        <span>Downloads</span>
+        <span>${item.likes}</span>
+        <span>${item.views}</span>
+        <span>${item.comments}</span>
+        <span>${item.downloads}</span>
+      </div>
+      </a>
+  </li>`
+    )
+    .join('');
 
-function showLoader() {}
+  galleryList.innerHTML = htmlImage;
 
-function hideLoader() {}
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  galleryList.innerHTML = '';
+}
+
+export function showLoader() {
+  loader.classList.remove('hidden');
+  console.log(loader);
+}
+
+export function hideLoader() {
+  //loader.classList.add('hidden');
+}
